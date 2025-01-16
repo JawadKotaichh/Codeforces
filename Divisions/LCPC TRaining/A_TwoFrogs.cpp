ee1d743c52c8cabc5d3f7fb6ca8b6b5162d8d1c1
Jawad Kotaich <155095794+JawadKotaichh@users.noreply.github.com>
2025-01-16 19:31:07 +0200

    new

---
+++ Divisions/LCPC TRaining/A_TwoFrogs.cpp
@@ -0,0 +1,28 @@
+# include<bits/stdc++.h>
+# define forn(n,i) for (int i=0;i<n;i++)
+# define v vector
+using namespace std;
+
+
+/*
+    We are gievn an array a from 1 to n
+    Alice starts then Bob
+    
+*/
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n,a,b;
+        cin>>n>>a>>b;
+        int maxi=max(a,b);
+        int mini=min(a,b);
+        if ((maxi-mini)%2==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+        
+    }
+    return 0;
+}
