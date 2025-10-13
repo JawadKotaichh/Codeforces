27b87aeaef0cb0b22c4531537ceef950bdf5c811
Jawad Kotaich <jawadkotaich@gmail.com>
2025-10-13 12:01:18 +0300

    dp

---
+++ Leetcode/dp/jump_game_2.py
@@ -0,0 +1,16 @@
+def jump(nums):
+    n = len(nums)
+    jumps = 0
+    current_end = 0
+    farthest = 0
+
+    for i in range(n - 1):
+        farthest = max(farthest, i + nums[i])
+        if i == current_end:
+            jumps += 1
+            current_end = farthest
+
+            if current_end >= n - 1:
+                break
+
+    return jumps
